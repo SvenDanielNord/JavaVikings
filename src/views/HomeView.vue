96d7dc4bb0b12d01e318a7524ddf52c89bc52dfd
Rasmus <rashed1110@skola.goteborg.se>
2023-04-25 14:48:21 +0200

    Working on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script>
 import { RouterLink } from 'vue-router'
+import { searchForThing } from '../data/FetchData';
+
+async function fetchUrl() {
+    let response = await searchForThing('mössa')
+    let json = response.json
+    console.log(json)
+    return json
+}
+
+//let fetch = {
+//    let response = await searchForThing('mössa')
+//}
+
 
 export default {
     components: {
         RouterLink
+    },
+    data() {
+        return {
+            url: fetchUrl()
+        }
+    },
+    methods: {
+
     }
 }
 </script>
@@ -11,6 +32,7 @@ export default {
 <template>
     <div>
         <p>Hello!</p>
-        <RouterLink to="/about">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        {{ url }}
     </div>
 </template>

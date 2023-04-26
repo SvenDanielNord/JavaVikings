016ff89495a07c991f0f9e1a88af57bee90e3b67
RasmusHed <rashed1110@skola.goteborg.se>
2023-04-26 19:05:20 +0200

    Adding hover method

--- src/components/PictureBox.vue
+++ src/components/PictureBox.vue
@@ -8,17 +8,31 @@ export default{
     data() {
         return {
             url: null,
+            title: null,
         }
     },
     methods: {
         async fetchUrl() {
             let response = await searchForThing('mössa')
             this.url = response.url
+            this.title = response.description
+        },
+        checkDescription(description) {
+            if (typeof description === 'object') {
+                this.title = description['@value']
+            } else if (description.length < 2) {
+                //TODO: Gör bättre
+                this.title = 'Missing information'
+            } else {
+                this.title = description
+            }
         }
     },
     async mounted() {
         let response = await searchForThing('mössa')
         this.url = response.url
+        this.description = response.description
+        this.checkDescription(this.description)
     }
     
 
@@ -26,6 +40,6 @@ export default{
 
 </script>
 <template>
-    <div><a @click="fetchUrl"><img :src="this.url" alt=""></a></div>
+    <div><a @click="fetchUrl" :title="title"><img :src="this.url" alt=""></a></div>
 </template>
 

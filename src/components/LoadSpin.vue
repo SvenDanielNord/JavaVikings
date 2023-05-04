4efbf13f93fa6c0bed6c31aa62490f330728519c
SvenDanielNord <svenor0701@skola.goteborg.se>
2023-05-04 14:14:09 +0200

    added loading pic

---
+++ src/components/LoadSpin.vue
@@ -0,0 +1,37 @@
+<script>
+export default{
+   name: "LoadSpin"
+
+}
+
+
+</script>
+
+<template>
+  <div class="loader"></div>
+</template>
+
+<style scoped>
+
+.loader {
+  width: 150px; /* control the size */
+  aspect-ratio: 1;
+  display: grid;
+  cursor:pointer;
+}
+.loader,
+.loader:before {
+  --_g: linear-gradient(#17177c 0 0) 50%; /* update the color here */
+  background: 
+    var(--_g)/34% 8%  space no-repeat,
+    var(--_g)/8%  34% no-repeat space;
+}
+.loader:before {
+  content: "";
+  transition:.5s;
+}
+.loader:hover:before {
+  transform: rotate(45deg);
+}
+
+</style>
--- src/components/PictureBox.vue
+++ src/components/PictureBox.vue
@@ -3,44 +3,54 @@
 
 <script>
 import { searchForThing } from '../data/FetchData.js'
+import LoadSpin from './LoadSpin.vue'
 
 export default {
-    props: ['search'],
+    components: { 
+        LoadSpin 
+    },
+    props: ["search"],
     data() {
         return {
             url: null,
             title: null,
-        }
+            loading: false,
+        };
     },
     methods: {
         async fetchUrl(search) {
-            let response = await searchForThing(search)
-            this.url = response.url
-            this.checkDescription(response.description)
+            this.loading = true;
+            let response = await searchForThing(search);
+            this.loading = false;
+            this.url = response.url;
+            this.checkDescription(response.description);
         },
         checkDescription(description) {
-            if (typeof description === 'object') {
-                this.title = description['@value']
-            // } else if (description.length < 2) {
-            //     //TODO: Gör bättre
-            //     this.title = 'Missing information'
-            } else {
-                this.title = description
+            if (typeof description === "object") {
+                this.title = description["@value"];
+                // } else if (description.length < 2) {
+                //     //TODO: Gör bättre
+                //     this.title = 'Missing information'
+            }
+            else {
+                this.title = description;
             }
         }
     },
     async mounted() {
-        let response = this.fetchUrl(this.search)
-        this.url = response.url
-        this.description = response.description
-        this.checkDescription(response.description)
-    }
-
-
+        let response = this.fetchUrl(this.search);
+        this.url = response.url;
+        this.description = response.description;
+        this.checkDescription(response.description);
+    },
+    
 }
 
 </script>
 <template>
-    <div><a @click="fetchUrl(search)" :title="title"><img :src="this.url" alt=""></a></div>
+    <div v-if="loading"><LoadSpin /></div>
+    <div v-else><a @click="fetchUrl(search)" :title="title"><img :src="this.url" alt=""></a>
+       
+    </div>
 </template>
 

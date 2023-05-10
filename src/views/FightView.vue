b17e29e379dbf8ecaf99b1ade66bd18009e376f8
RasmusHed <rashed1110@skola.goteborg.se>
2023-05-10 22:24:47 +0200

    Adding the first real fight mechanic

--- src/views/FightView.vue
+++ src/views/FightView.vue
@@ -3,10 +3,16 @@ import { RouterLink } from 'vue-router'
 import LoadCharacters from '../components/LoadCharacter.vue'
 
 export default {
+    data() {
+        return {
+            location: null,
+            fighter: null,
+        }
+    },
     components: {
         RouterLink,
         LoadCharacters
-    },
+    }
 }
 </script>
 

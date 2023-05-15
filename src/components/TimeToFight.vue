94715e4827530a9ffa73b50ed6a1faa3127a705b
RasmusHed <rashed1110@skola.goteborg.se>
2023-05-15 14:56:39 +0200

    Adding a lot more variations to the fight text

--- src/components/TimeToFight.vue
+++ src/components/TimeToFight.vue
@@ -4,18 +4,99 @@ import { getLocation } from '../data/FetchLocation.js'
 export default {
     data() {
         return {
-            location: '',
-            winner: '',
-            loser: '',
+            location: null,
+            winner: null,
+            loser: null,
             fighterOne: {
-                name: '',
-                stats: 0
+                name: null,
+                stats: null
             },
             fighterTwo: {
-                name: '',
-                stats: 0
+                name: null,
+                stats: null
             },
-            trigger: false
+            trigger: false,
+            partOneMessages: [
+                "In the midst of a serene afternoon in",
+                "Under the clear blue sky of",
+                "Amidst the serene beauty of",
+                "In a peaceful corner of",
+                "Surrounded by tranquility in"
+            ],
+            partTwoMessages: [
+                "relishes the tranquility until an unexpected encounter with",
+                "basks in the peaceful ambiance until the unexpected arrival of",
+                "enjoys a moment of serenity until",
+                "finds solace in the calmness until",
+                "is at peace until"
+            ],
+            partThreeMessages: [
+                "shatters the silence.",
+                "sparks tension in the air.",
+                "breaks the calmness.",
+                "disturbs the peaceful atmosphere.",
+                "disrupts the tranquility."
+            ],
+            partFourMessages: [
+                "Fixing their gaze upon one another, both fighters lock eyes, their hands instinctively reaching for their swords.",
+                "Their eyes meet in a moment of silent understanding as they simultaneously unsheathe their blades, preparing for the impending clash.",
+                "With an intense stare, the fighters engage in a wordless exchange, drawing their swords in perfect synchronization.",
+                "Their eyes lock in a fierce gaze as they prepare for an intense battle, their weapons ready.",
+                "As tension builds, the fighters share a determined look, knowing what lies ahead."
+            ],
+            partFiveMessages: [
+                "Swift and precise,",
+                "With a sudden burst of energy,",
+                "Quick and agile,",
+                "In a graceful motion,",
+                "Like a whirlwind,"
+            ],
+            partSixMessages: [
+                "executes a calculated strike, momentarily disrupting",
+                "delivers a staggering blow that momentarily staggers",
+                "lands a well-aimed attack, momentarily disorienting",
+                "launches a powerful assault, momentarily overwhelming",
+                "strikes with precision, momentarily throwing off"
+            ],
+            partSevenMessages: [
+                "'s balance. Yet,",
+                ". But",
+                "'s equilibrium. However,",
+                ". Nonetheless,",
+                "'s stability. Still,"
+            ],
+            partEightMessages: [
+                "swiftly regains composure and retaliates with a swift counterattack.",
+                "swiftly recovers, launching a relentless counter-assault.",
+                "proves their resilience, quickly retaliating with a formidable strike.",
+                "counterattacks with lightning speed, catching the opponent off guard.",
+                "recovers swiftly and counterstrikes with exceptional agility."
+            ],
+            partNineMessages: [
+                "Following a grueling and monotonous battle,",
+                "In an arduous and tedious struggle,",
+                "After enduring a prolonged and mind-numbing duel,",
+                "Having fought relentlessly in an exhausting encounter,",
+                "In a battle that seemed never-ending,"
+            ],
+            partTenMessages: [
+                "emerges triumphant, ending the fight by severing the head of",
+                "ultimately prevails, putting an end to the fight by beheading",
+                "triumphs over the opponent, leaving them defeated",
+                "prevails in the fight, proving their superiority against",
+                "claims victory, bringing an end to the battle with"
+            ],
+
+            partOne: null,
+            partTwo: null,
+            partThree: null,
+            partFour: null,
+            partFive: null,
+            partSix: null,
+            partSeven: null,
+            partEight: null,
+            partNine: null,
+            partTen: null
         }
     },
     props: {
@@ -23,9 +104,10 @@ export default {
         characterTwo: {}
     },
     methods: {
-        test() {
+        startFight() {
             this.testLocation()
             this.totalStats()
+            this.setText()
         },
         async testLocation() {
             this.location = await getLocation()
@@ -48,9 +130,51 @@ export default {
             this.loser = this.fighterOne.stats > this.fighterTwo.stats ? this.fighterTwo.name : this.fighterOne.name;
         },
         triggerText() {
-            console.log(this.trigger)
             this.trigger = true
-            console.log(this.trigger)
+        },
+        setPartOne() {
+            console.log(this.partOneMessages[Math.floor(Math.random() * this.partOneMessages.length)])
+            this.partOne = this.partOneMessages[Math.floor(Math.random() * this.partOneMessages.length)]
+        },
+        setPartTwo() {
+            this.partTwo = this.partTwoMessages[Math.floor(Math.random() * this.partTwoMessages.length)]
+        },
+        setPartThree() {
+            this.partThree = this.partThreeMessages[Math.floor(Math.random() * this.partThreeMessages.length)]
+        },
+        setPartFour() {
+            this.partFour = this.partFourMessages[Math.floor(Math.random() * this.partFourMessages.length)]
+        },
+        setPartFive() {
+            this.partFive = this.partFiveMessages[Math.floor(Math.random() * this.partFiveMessages.length)]
+        },
+        setPartSix() {
+            this.partSix = this.partSixMessages[Math.floor(Math.random() * this.partSixMessages.length)]
+        },
+        setPartSeven() {
+            this.partSeven = this.partSevenMessages[Math.floor(Math.random() * this.partSevenMessages.length)]
+        },
+        setPartEight() {
+            this.partEight = this.partEightMessages[Math.floor(Math.random() * this.partEightMessages.length)]
+        },
+        setPartNine() {
+            this.partNine = this.partNineMessages[Math.floor(Math.random() * this.partNineMessages.length)]
+        },
+        setPartTen() {
+            this.partTen = this.partTenMessages[Math.floor(Math.random() * this.partTenMessages.length)]
+        },
+        setText() {
+            this.setPartOne()
+            this.setPartTwo()
+            this.setPartThree()
+            this.setPartFour()
+            this.setPartFive()
+            this.setPartSix()
+            this.setPartSeven()
+            this.setPartEight()
+            this.setPartNine()
+            this.setPartTen()
+            this.triggerText()
         }
     }
 }
@@ -58,12 +182,13 @@ export default {
 
 <template>
     <div>
-        <button @click="test(); triggerText()">Fight!</button>
+        <button @click="startFight()">Fight!</button>
         <div v-if="trigger" class="letMeSeeSomething">
-            {{ fighterOne.name }} is enjoying a perfectly calm, nice sunny day in {{ location }} when suddenly {{ fighterTwo.name }} shows up.<br>
-            They look each other deep in the eyes and draw thier swords. <br>
-            {{ fighterOne.name }} put's {{ fighterTwo.name }} off balance with his swing, but {{ fighterTwo.name }} recovers quick and hits back. <br>
-            After a long and extremly boring fight {{ winner }} secrues the victory by beheading {{ loser }}
+            {{ partOne }} {{ location }}, {{ fighterOne.name }} {{ partTwo }} {{ fighterTwo.name }} {{ partThree }}<br>
+            {{ partFour }}
+            {{ partFive }} {{ fighterOne.name }} {{ partSix }} {{ fighterTwo.name }} {{ partSeven }} {{ fighterTwo.name }}
+            {{ partEight }}
+            {{ partNine }} {{ winner }} {{ partTen }} {{ loser }}
         </div>
     </div>
 </template>

3211454d81cab619026e3bd3ab116b530b064b62
SvenDanielNord <svenor0701@skola.goteborg.se>
2023-05-17 11:01:22 +0200

    Add system-test

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,18 +1,57 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Easy test', () => {
-  it('visits the root url and /gear then goes back', () => {
+describe('path test', () => {
+  it('visits gear via generate gear button', () => {
     cy.visit('/')
-    cy.get('.menu> div:nth-child(2) > a > button').click()
-    cy.location('pathname').should('eq', '/gear')
-    // cy.get('.menu> div:nth-child(1) > a > button').click()
-    cy.get('input').type('Jocke')
     
-    cy.get('.mb-3 >  button').click()
+    cy.get('.menu > div:nth-child(3) > a > button').click()
+    cy.location('pathname').should('eq', '/gear')
+    
+    })
 
-    cy.get('.menu> div:nth-child(3) > a > button').click()
-    cy.get('.p1 > button').click()
-    cy.get('li').contains('Jocke')
+    it('Checks if player is saved', () => {
+      
+       
+      cy.visit('/gear')
 
+      cy.get('#gear[src^="http"]').should('have.length', 6)
+
+      cy.get('input').type('Jocke')
+      
+      cy.get('.mb-3 >  button').click()
+      
+      cy.get('.menu> div:nth-child(4) > a > button').click()
+      
+      cy.get('button').contains('Fighter 1').click()
+
+      cy.get('li').contains('Jocke').click()
+
+      cy.get('img').each((item, list) => {
+        console.log(list)
+        cy.wrap(item).should('have.attr', 'src').should('not.eq', '')
+
+      })
+
+    })
+       
+     
   })
-})
+  
+  
+  // cy.get('img').should('have.attr', 'src').should('not.eq', '')
+
+  // cy.get('').each(($li) => cy.log($li.text()))
+
+  // cy.get('[src = attr]').should('not.eq', '')
+
+  // cy.get('img').should('have.attr', 'src').then(($src) =>{
+
+  // expect($src.val()).not.to.eq('') 
+
+    
+  // })
+  
+  // cy.get('[src]').should('not.have.value', '')
+
+  // cy.get('img').should('have.attr', 'src').and('not.eq', '')
+  // cy.get('img').should('have.attr', 'src').and('be.empty')

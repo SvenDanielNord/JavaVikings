1233dff72c6d85352ee331e9cc7cb819a5253037
JoakimNiemer <joakimniemer@gmail.com>
2023-05-28 17:52:46 +0200

    mergeconflict fixed

--- src/views/GearView.vue
+++ src/views/GearView.vue
@@ -45,15 +45,15 @@ export default {
                 this.saveAlert = false;
             }, 3000);
         },
-        checkLoad(){
-            
-                this.countPicBox++
-            
-            if (this.countPicBox > 5) {     
+        checkLoad() {
+
+            this.countPicBox++
+
+            if (this.countPicBox > 5) {
                 this.loadSome = false;
             }
         },
-        
+
         clearCharacter() {
             localStorage.clear()
         },
@@ -84,15 +84,12 @@ export default {
 
         <div class="row mt-3">
             <div class="col-12">
+
                 <div id="save" class="mb-3 input-group justify-content-center">
-                    <button button class="btn btn-danger " @click="saveCharacter">Save</button>
-
-
-                <div  id="save" class="mb-3 input-group justify-content-center">
                     <!-- Borde detta vara en form? -->
-                    <button v-if="this.loadSome" disabled="true" button class="btn btn-danger " @click="saveCharacter">Save</button>
+                    <button v-if="this.loadSome" disabled="true" button class="btn btn-danger "
+                        @click="saveCharacter">Save</button>
                     <button v-else button class="btn btn-danger " @click="saveCharacter">Save</button>
-
                     <input type="text" class="form-group" id="name" v-model="characterName" required minlength="4"
                         maxlength="16" size="11" placeholder="Character name" aria-label="Save"
                         aria-describedby="basic-addon1">
@@ -104,6 +101,7 @@ export default {
         <div class="row text-center justify-content-center">
             <div v-if="saveAlert" class="col-5 saveAlertMessage"> Character saved!</div>
         </div>
+        
     </div>
 
 
@@ -113,15 +111,21 @@ export default {
 
 
     <div class="container col-12 text-center">
-        <PictureBox @loads="() => checkLoad()"  @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemHelmet" search="hjälm" @load="console.log(5)" />
+        <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemHelmet"
+            search="hjälm" @load="console.log(5)" />
         <div class="row justify-content-center row-cols-auto">
-            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemSword mt-4" search="svärd" />
-            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemArmor mt-4" search="tröja" />
-            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemShield mt-4" search="sköld" />
+            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)"
+                class="itemSword mt-4" search="svärd" />
+            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)"
+                class="itemArmor mt-4" search="tröja" />
+            <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)"
+                class="itemShield mt-4" search="sköld" />
         </div>
 
-        <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemLeggs mt-4" search="byxa" />
-        <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)" class="itemFeet mt-4" search="skor" />
+        <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)"
+            class="itemLeggs mt-4" search="byxa" />
+        <PictureBox @loads="() => checkLoad()" @stat="(stat, name, url) => updateItems(stat, name, url)"
+            class="itemFeet mt-4" search="skor" />
     </div>
 </template>
 
@@ -134,5 +138,4 @@ export default {
     color: rgb(224, 44, 44);
     background-color: whitesmoke;
     border-radius: 0.5rem;
-}
-</style>
+}</style>

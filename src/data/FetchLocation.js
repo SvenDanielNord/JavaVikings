5c839a90ecf367ad9dbfee1d1cdb2cf56f53e146
RasmusHed <rashed1110@skola.goteborg.se>
2023-05-29 17:00:01 +0200

    Adding docs to FetchLocation

--- src/data/FetchLocation.js
+++ src/data/FetchLocation.js
@@ -1,3 +1,7 @@
+/**
+ * Fetches 20 different locations and returns one of those randomly
+ * @returns A random location from ksamsök API
+ */
 async function getLocation() {
     const apiUrl = `https://kulturarvsdata.se/ksamsok/api?method=search&query=geoDataExists=j&hitsPerPage=20`
     let response = await fetch(apiUrl, {
@@ -12,7 +16,9 @@ async function getLocation() {
     let records = data.result.records
     return getRecords(records)
 }
-
+/**
+ * If we get 400/500 as response we throw an error with message that we catch and alert in TimeToFight 
+ */
 function readErrorResponseAPI(resp) {
     if (resp.status >= 500) {
       throw new Error("Something is wrong with the server");
@@ -20,7 +26,10 @@ function readErrorResponseAPI(resp) {
       throw new Error("Samsök is not responding");
     }
   }
-
+/**
+ * If we get 200 as response we process it and return a location 
+ * @returns A random location
+ */
 function getRecords(records) {
     if (records.length > 0) {
         let record = records[Math.floor(Math.random() * records.length)]
